d369dd9e1b68286f8b562fef485fda4a06d48db6
DarkHorse1919 <xiaohm1983@gmail.com>
2015-08-26 21:13:11 +0800

    Demo show planned

---
+++ iOS_Good_Practices/iOS_Good_Practices/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS_Good_Practices
+//
+//  Created by ShenzhenSenink technology Co.,LTD on 15/8/26.
+//  Copyright (c) 2015年 KevinHM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
